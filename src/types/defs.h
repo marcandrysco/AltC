872bca1d723148964606d933d268a377b0e18628
Marc Andrysco <marc.andrysco@gmail.com>
2015-04-20 15:31:05 -0700

    added missing avltree definitions

--- src/types/defs.h
+++ src/types/defs.h
@@ -24,4 +24,75 @@ typedef void (*delete_f)(void *ptr);
 
 typedef int (*compare_f)(const void *left, const void *right);
 
+
+/**
+ * AVL tree root structure.
+ *   @count: The number of nodes.
+ *   @node: The root node.
+ *   @compare: The comparison callback.
+ */
+
+struct avltree_root_t {
+	unsigned int count;
+	struct avltree_node_t *node;
+
+	compare_f compare;
+};
+
+/**
+ * AVL tree node storage.
+ *   @balance: The current balance of the node, between '-2' to '2'.
+ *   @ref: The reference.
+ *   @parent, child: The parent and child nodes.
+ */
+
+struct avltree_node_t {
+	int8_t balance;
+
+	void *ref;
+	struct avltree_node_t *parent, *child[2];
+};
+
+/**
+ * Generic AVL tree.
+ *   @root: The root.
+ *   @delete: Deletion function.
+ */
+
+struct avltree_t {
+	struct avltree_root_t root;
+
+	delete_f delete;
+};
+
+/**
+ * Generic AVL tree instance.
+ *   @ref: The stored reference.
+ *   @delete: Delete function.
+ *   @node: The tree node.
+ */
+
+struct avltree_inst_t {
+	void *ref;
+	delete_f delete;
+
+	struct avltree_node_t node;
+};
+
+
+/**
+ * String trie node structure.
+ *   @ref: The stored reference.
+ *   @str: The string piece.
+ *   @tree: The subtree.
+ *   @node: The node.
+ */
+
+struct strtrie_t {
+	void *ref;
+	char *str;
+	struct avltree_root_t tree;
+	struct avltree_node_t node;
+};
+
 #endif
